ff503510a04b3d0ad7cb93047078e3818ef582c4
Pablo Ruan <pabloruan@outlook.com>
2015-12-16 19:59:06 -0200

    Initial Commit

---
+++ Share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Share
+//
+//  Created by Pablo on 12/16/15.
+//  Copyright © 2015 PrimeSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
